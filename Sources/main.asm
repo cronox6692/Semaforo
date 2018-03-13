a4c22edde63c05e34ded0a4ea5950593c05d5d30
nickdpz <ndpastranz@correo.udistrital.edu.co>
2018-03-13 15:44:46 -0500

    Configuracion de Puertos

--- Sources/main.asm
+++ Sources/main.asm
@@ -2,24 +2,39 @@
 	
 LOCK	EQU		6; Verificar el enganche del reloj
 		ORG 	0B0H; Direccion de memoria de variables	
-CONTROL	DS	1; Definir control como variable con posiciones de memoria byte
+
+AUTOSET DS 1
+V1      DS 1
+A1      DS 1
+R1      DS 1 
+V2 		DS 1
 
 		ORG		0C000H; Direccion de memoria para programa
 INICIO:
-		CLR		CONTROL; Limpiar control es bueno limpiar las variables 
-		CLRA ;Limpiar acumulador
-		STA		SOPT1; Desbilitar
-		MOV		#40H,MCGC2; Que oscilador tomar
-		MOV		#6,MCGC1; Divisor de frecuencia 
-		BRCLR	LOCK,MCGSC,* ; Espere a que quede a 8 Mhz
-		LDHX	#4B0H ;Reubicar pila
-		TXS ;Lo que esta en x a la posicion de la pila
+
+		CLRA 				      ;Limpiar acumulador
+		STA		SOPT1		      ;Desbilitar
+		LDHX	#4B0H 		      ;Reubicar pila
+		TXS 			          ;Lo que esta en x a la posicion de la pila
+		MOV		#0AAH,MCGTRM;	
+		MOV		#6,MCGC1		  ;Ajuste frecuencia del oscilador interno 
+		BRCLR	LOCK,MCGSC,*      ;Espere a que el oscilador se enganche
+		MOV		#07H,PTBDD        ;PUERTO B SALIDA 3 PINES DE SALIDA   00000111
+		LDA		#00011000         ;PARA RESISTENCIA DE PULL-UP  00011000
+		STA		PTBPE 			  ;PUERTO E ENTRADA /
+
+
+		CLR 	AUTOSET 
+		MOV 	#6H, V1
+		MOV 	#5H, A1
+		MOV 	#4H, R1
+		MOV 	#2H, V2
 		
-		MOV		#7FH,PTBDD ;PUERTO B SALIDA 6 PINES DE SALIDA LED1-LED6
-		MOV		#7FH,PTCDD ;PUERTO C SALIDA 6 PINES DE SALIDA LED7-LED12
-		MOV		#7FH,PTGDD ;PUERTO B SALIDA 6 PINES DE SALIDA LED13-LED16
-		MOV		#7FH,PTFDD ;PUERTO F SALIDA 4 PINES DE SALIDA TIERRAS
-		MOV		#00H,PTEDD ;PUERTO E NO-SALIDA 8 PINES DE 8 00H 
-		LDA		#7FH
-		STA		PTEPE ; PUERTO E ENTRADA
+LOOP:	JSR     SUBRUTINA_SEMAFORO
+		LDA 	PTBD
+		AND 	#00011000
+		
+		
+		
+		
 ;------------CONFIGURACION TIERRAS----
