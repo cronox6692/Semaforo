91617486c2a779472946c2c2381d0167fb4162a7
Francisco Lopez Puyo <francisco-9708@hotmail.es>
2018-03-16 18:45:27 -0500

    Arreglo reloj

--- Sources/main.asm
+++ Sources/main.asm
@@ -1,12 +1,12 @@
 	INCLUDE 'MC9S08JM16.INC' 
 	
-LOCK	EQU		6; Verificar el enganche del reloj
+LOCK	EQU		6    ; Verificar el enganche del reloj
 LEDV	EQU     2
 LEDA    EQU     3
 LEDR    EQU     4
 
-		ORG 	0B0H; Direccion de memoria de variables	
-
+		ORG 	0B0H  ;Direccion de memoria de variables
+			
 AUTOSET DS 1
 V1      DS 1
 A1      DS 1
@@ -16,8 +16,8 @@ AUX     DS 1
 CONT    DS 1 
 
 		ORG		0C000H; Direccion de memoria para programa
-INICIO:
-		CLRA 				      ;Limpiar acumulador
+
+INICIO:	CLRA 				      ;Limpiar acumulador
 		STA		SOPT1		      ;Desbilitar
 		LDHX	#4B0H 		      ;Reubicar pila
 		TXS 			          ;Lo que esta en x a la posicion de la pila
@@ -27,14 +27,12 @@ INICIO:
 		MOV		#00011100B,PTBDD  ;PUERTO B SALIDA 3 PINES DE SALIDA   
 		LDA		#00000011B        ;PARA RESISTENCIA DE PULL-UP  00011000
 		STA		PTBPE 			  ;PUERTO B ENTRADA /
-
-
 		CLR 	AUTOSET           ;--------------------------
 		MOV 	#6H, V1
 		MOV 	#5H, AUX           ; Valores de la secuencia 1
 		MOV 	#4H, R1
-		MOV 	#2H, V2           ;--------------------------
-		
+		MOV 	#1H, A1
+		MOV 	#2H, V2           ;--------------------------	
 LOOP:	JSR     S_SEM 
 		LDA 	PTBD
 		AND 	#00000011B
@@ -54,26 +52,25 @@ SALTO_A:LDA 	PTBD              ;-----------------------------------
 		AND 	#00000011B        ; Se hace un ciclo while infinito hasta que 
 		CBEQA   #3H,VALORES		  ; se suelte el boton TEMP
 		JMP     SALTO_A           ;-----------------------------------------
-
 VALORES:LDA     V1                ; Carga en acomulador el valor de V1
-		CBEQA   #6,VALORES_2      ; Si el tiempo de verde es 6s salta a rutina 
+		CBEQA   #6H,VALORES_2      ; Si el tiempo de verde es 6s salta a rutina 
 		MOV 	#6H, V1
 		MOV 	#5H, AUX
 		MOV 	#4H, R1
+		MOV 	#1H, A1
 		MOV 	#2H, V2
 		JMP     LOOP
-		
 VALORES_2:MOV 	#4H, V1
 		MOV 	#4H, AUX
 		MOV 	#6H, R1
+		MOV 	#1H, A1
 		MOV 	#3H, V2
-		JMP     LOOP
-		
+		JMP     LOOP	
 ;--------------------------------------------------------------
 ;-----------------SUBRUTINA SEMAFORO---------------------------
 ;--------------------------------------------------------------
 S_SEM:  LDA      AUTOSET
-	    CBEQA    #1,MANUAL
+	    CBEQA    #1H,MANUAL
 	    BSET     LEDV,PTBD
 	    LDA      V1
 	    JSR      SUB_T
@@ -90,35 +87,34 @@ S_SEM:  LDA      AUTOSET
 	    JSR      SUB_T
 	    BCLR     LEDV,PTBD
 	    BCLR     LEDA,PTBD
-	    RTS                         ; RETORNA A LOOP
-	    
-MANUAL: CLR      CONT
-		CLRA
-		BSET     LEDV,PTBD
-LEER:	LDA      PTBD
-		AND 	 #00000011B
-		CBEQA    #1H, REG2
-		CBEQA    #0H, LEER
-		CBEQA    #3H, LEER
-		;SI ES 2 SIGUE
-ESPERA:	LDA 	 PTBD
-		AND      #00000011B
-		CBEQA	 #3H,M_M
-		JMP      ESPERA
-M_M:	LDA 	 CONT
-		ADD      #1H
-		STA      CONT
-PRE:	CBEQA    #1H, MODO1
+	    RTS                         ; RETORNA A LOOP    
+MANUAL: CLR      CONT               ; Limpia contador
+		CLRA                        ; Inicializa el contador
+		BSET     LEDV,PTBD          ; PRENDE LED VERDE
+LEER:	LDA      PTBD               ; LEE puerto
+		AND 	 #00000011B         ; Enmasara puerto
+		CBEQA    #1H, REG2          ; Si oprime auto --> salta a REG2
+		CBEQA    #3H, LEER          ; si no se oprime nada , vulve a preguntar
+		CBEQA    #0H, LEER          ; si oprime los dos vulve a preguntar
+		;SI ES 2 --> oprimio pasar al siguiente led
+ESPERA:	LDA 	 PTBD             ; lee puerto
+		AND      #00000011B       ; enmascara
+		CBEQA	 #3H,M_M          ; si suelta el boton pasa a modo manual
+		JMP      ESPERA           ;
+M_M:	LDA 	 CONT             ; incrementa en 1 contador
+		ADD      #1H              ;
+		STA      CONT             ;
+PRE:	LDA		 CONT
+		CBEQA    #1H, MODO1
 		CBEQA    #2H, MODO2
 		CBEQA    #3H, MODO3
 		CLR      CONT
 		BSET     LEDV,PTBD
-		JMP      LEER
-		
+		JMP      LEER	
 MODO1:  BCLR     LEDV,PTBD
 		JSR      SUB_P
 		LDA      AUTOSET
-		CBEQA    #0,AUTO2
+		CBEQA    #0H,AUTO2
 		JMP      PRE
 MODO2:  BSET     LEDR,PTBD
 		JMP      LEER 
@@ -126,49 +122,50 @@ MODO3:  BCLR     LEDR,PTBD
 		BSET     LEDV,PTBD
 		BSET     LEDA,PTBD
 		JMP      LEER				  
-
 REG2:   LDA 	 PTBD
-		AND 	 #00000011B
+		AND 	 #00000011B			;espera que lo suelte
 		CBEQA	 #3H, AUTO2
 		JMP      REG2
 AUTO2:	CLR 	 AUTOSET
 		MOV 	#6H, V1
 		MOV 	#5H, AUX
 		MOV 	#4H, R1
+		MOV 	#1H, A1
 		MOV 	#2H, V2
 		BCLR 	LEDV, PTBD
 		BCLR	LEDA, PTBD
 		BCLR	LEDR, PTBD
-		RTS		
-
 		RTS	                        ; RETORNA A LOOP
-		
 ;--------------------------------------------------------------
 ;-----------------SUBRUTINA TIEMPO---------------------------
 ;--------------------------------------------------------------
-SUB_T:	LDX     #20         ; Carga x con 20
+SUB_T:	LDX     #14H        ; Carga x con 20
 		MUL                 ; se multiplica Acomulador con 20 para obtener tiempo en S
 TIEMPO: LDHX    #50000D     ; Tiempo de 50ms
-CICLOT: AIX		#-1         ; pierde tiempo
-		CPHX	#0          ; compara HX con 0
+CICLOT: AIX		#-1D         ; pierde tiempo
+		CPHX	#0H          ; compara HX con 0
 		BNE		CICLOT      ; Si hx es igual a 0 sigue
 		DBNZA   TIEMPO      ; si acomulador es igual a 0 sigue 
+		LDA     #1H          ; carga acomulador con 1 para 1s
 		RTS                 ; retorna
 ;--------------------------------------------------------------
 ;-----------------SUBRUTINA PARPADEO---------------------------
 ;--------------------------------------------------------------
 	
-SUB_P:  LDA     AUTO        ; carga valor de auto 
-		CBEQA   #1,P_M      ; pregunta valor de auto (modo)
-		LDA     #1          ; carga acomulador con 1 para 1s
+SUB_P:  LDA     AUX
+		PSHA  
+		LDA     AUTOSET        ; carga valor de auto 
+		CBEQA   #1H,P_M      ; pregunta valor de auto (modo)
+		LDA     #1H          ; carga acomulador con 1 para 1s
 CICLO_P:BSET    LEDA,PTBD   ; prende led amarillo
 		JSR     SUB_T       ; espera 1s
 		BCLR    LEDA,PTBD   ; apaga led amarillo
 		JSR     SUB_T       ; espera 1s
 		DBNZ    AUX,CICLO_P ; pregunta cantidad de parpadeos , si faltan salta
+		PULA
+		STA     AUX			; Se obtiene de nuevo el valor de aux
 		RTS
-		
-P_M:    LDA     #1          ; carga acomulador con 1 para 1s
+P_M:    LDA     #1H         ; carga acomulador con 1 para 1s
 		BSET    LEDA,PTBD   ; Prende amarillo
 		JSR     SUB_T       ; espera 1s
 		BCLR    LEDA,PTBD   ; apaga led amarillo
@@ -177,15 +174,15 @@ P_M:    LDA     #1          ; carga acomulador con 1 para 1s
 		AND 	#00000011B  ; Enmascarar 
 		CBEQA   #0H,P_M	    ; Si no oprime nada sigue parpadeando
 		CBEQA   #3H,P_M     ; si oprime los dos pulsadores sigue parpandeando
-		CBEQA   #1H,REG
+		CBEQA   #1H,REG		;
 		;SI ES IGUAL A 2
 SOST_P: LDA 	PTBD              
 		AND 	#00000011B        
 		CBEQA   #3H,SOLT_P 
 		JMP     SOST_P           
-SOLT_P: LDA     #1
+SOLT_P: LDA     #1H
 		ADD     CONT
-		STA     CONT        
+		STA     CONT              ; contador igual a 2
 		RTS                       ; RETORNA A SUBRUTINA SEMAFORO
 REG:    LDA 	PTBD              ;-----------------------------------
 		AND 	#00000011B        ; Se hace un ciclo while infinito hasta que 
@@ -195,5 +192,5 @@ AUTO_REG:
 		MOV     #0H,AUTOSET       ; una vez se suelta el boton se carga autoset con 1
 		RTS                       ; regresa a subrutina semaforo
 		
-		ORG     0FFFFEH
+		ORG     0FFFEH
 		FDB		INICIO
